838a725d0ea7189e5cde388393c97db9aa7e01dc
igor <igor@igor.io>
2022-05-11 23:56:22 +0200

    -g0 for compact assembly

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ WORKDIR /usr/src/myapp
 # RUN gcc -o myapp main.c
 # CMD ["./myapp"]
 # RUN gcc -S main.c
-RUN gcc -g -S -O2 main.c
+RUN gcc -g -S -O3 -g0 main.c
 CMD ["cat", "main.s"]
--- main.s
+++ main.s
@@ -1,7 +1,6 @@
 	.arch armv8-a
 	.file	"main.c"
 	.text
-.Ltext0:
 	.section	.rodata.str1.8,"aMS",@progbits,1
 	.align	3
 .LC0:
@@ -13,38 +12,24 @@
 	.type	main, %function
 main:
 .LFB11:
-	.file 1 "main.c"
-	.loc 1 6 12 view -0
 	.cfi_startproc
-	.loc 1 7 3 view .LVU1
-	.loc 1 6 12 is_stmt 0 view .LVU2
 	stp	x29, x30, [sp, -16]!
 	.cfi_def_cfa_offset 16
 	.cfi_offset 29, -16
 	.cfi_offset 30, -8
-	.loc 1 7 3 view .LVU3
 	adrp	x0, .LC0
 	add	x0, x0, :lo12:.LC0
-	.loc 1 6 12 view .LVU4
 	mov	x29, sp
-	.loc 1 7 3 view .LVU5
 	bl	system
-.LVL0:
-	.loc 1 8 3 is_stmt 1 view .LVU6
-	.loc 1 8 4 is_stmt 0 view .LVU7
 	adrp	x2, .LANCHOR0
-	.loc 1 10 1 view .LVU8
 	mov	w0, 0
 	ldp	x29, x30, [sp], 16
 	.cfi_restore 30
 	.cfi_restore 29
 	.cfi_def_cfa_offset 0
-	.loc 1 8 4 view .LVU9
 	ldr	w1, [x2, #:lo12:.LANCHOR0]
 	add	w1, w1, 1
 	str	w1, [x2, #:lo12:.LANCHOR0]
-	.loc 1 9 3 is_stmt 1 view .LVU10
-	.loc 1 10 1 is_stmt 0 view .LVU11
 	ret
 	.cfi_endproc
 .LFE11:
@@ -52,348 +37,16 @@ main:
 	.global	i
 	.global	tape
 	.bss
-	.align	3
+	.align	4
 	.set	.LANCHOR0,. + 0
 	.type	i, %object
 	.size	i, 4
 i:
 	.zero	4
-	.zero	4
+	.zero	12
 	.type	tape, %object
 	.size	tape, 20000
 tape:
 	.zero	20000
-	.text
-.Letext0:
-	.file 2 "/usr/include/stdlib.h"
-	.section	.debug_info,"",@progbits
-.Ldebug_info0:
-	.4byte	0xf9
-	.2byte	0x4
-	.4byte	.Ldebug_abbrev0
-	.byte	0x8
-	.uleb128 0x1
-	.4byte	.LASF11
-	.byte	0xc
-	.4byte	.LASF12
-	.4byte	.LASF13
-	.4byte	.Ldebug_ranges0+0
-	.8byte	0
-	.4byte	.Ldebug_line0
-	.uleb128 0x2
-	.byte	0x8
-	.byte	0x7
-	.4byte	.LASF0
-	.uleb128 0x2
-	.byte	0x4
-	.byte	0x7
-	.4byte	.LASF1
-	.uleb128 0x3
-	.byte	0x4
-	.byte	0x5
-	.string	"int"
-	.uleb128 0x2
-	.byte	0x8
-	.byte	0x5
-	.4byte	.LASF2
-	.uleb128 0x2
-	.byte	0x8
-	.byte	0x5
-	.4byte	.LASF3
-	.uleb128 0x2
-	.byte	0x1
-	.byte	0x8
-	.4byte	.LASF4
-	.uleb128 0x2
-	.byte	0x2
-	.byte	0x7
-	.4byte	.LASF5
-	.uleb128 0x2
-	.byte	0x1
-	.byte	0x6
-	.4byte	.LASF6
-	.uleb128 0x2
-	.byte	0x2
-	.byte	0x5
-	.4byte	.LASF7
-	.uleb128 0x2
-	.byte	0x1
-	.byte	0x8
-	.4byte	.LASF8
-	.uleb128 0x2
-	.byte	0x8
-	.byte	0x7
-	.4byte	.LASF9
-	.uleb128 0x4
-	.4byte	0x37
-	.4byte	0x87
-	.uleb128 0x5
-	.4byte	0x29
-	.2byte	0x1387
-	.byte	0
-	.uleb128 0x6
-	.4byte	.LASF10
-	.byte	0x1
-	.byte	0x3
-	.byte	0x5
-	.4byte	0x76
-	.uleb128 0x9
-	.byte	0x3
-	.8byte	tape
-	.uleb128 0x7
-	.string	"i"
-	.byte	0x1
-	.byte	0x4
-	.byte	0x5
-	.4byte	0x37
-	.uleb128 0x9
-	.byte	0x3
-	.8byte	i
-	.uleb128 0x8
-	.4byte	.LASF14
-	.byte	0x1
-	.byte	0x6
-	.byte	0x5
-	.4byte	0x37
-	.8byte	.LFB11
-	.8byte	.LFE11-.LFB11
-	.uleb128 0x1
-	.byte	0x9c
-	.4byte	0xef
-	.uleb128 0x9
-	.8byte	.LVL0
-	.4byte	0xef
-	.uleb128 0xa
-	.uleb128 0x1
-	.byte	0x50
-	.uleb128 0x9
-	.byte	0x3
-	.8byte	.LC0
-	.byte	0
-	.byte	0
-	.uleb128 0xb
-	.4byte	.LASF15
-	.4byte	.LASF15
-	.byte	0x2
-	.2byte	0x30d
-	.byte	0xc
-	.byte	0
-	.section	.debug_abbrev,"",@progbits
-.Ldebug_abbrev0:
-	.uleb128 0x1
-	.uleb128 0x11
-	.byte	0x1
-	.uleb128 0x25
-	.uleb128 0xe
-	.uleb128 0x13
-	.uleb128 0xb
-	.uleb128 0x3
-	.uleb128 0xe
-	.uleb128 0x1b
-	.uleb128 0xe
-	.uleb128 0x55
-	.uleb128 0x17
-	.uleb128 0x11
-	.uleb128 0x1
-	.uleb128 0x10
-	.uleb128 0x17
-	.byte	0
-	.byte	0
-	.uleb128 0x2
-	.uleb128 0x24
-	.byte	0
-	.uleb128 0xb
-	.uleb128 0xb
-	.uleb128 0x3e
-	.uleb128 0xb
-	.uleb128 0x3
-	.uleb128 0xe
-	.byte	0
-	.byte	0
-	.uleb128 0x3
-	.uleb128 0x24
-	.byte	0
-	.uleb128 0xb
-	.uleb128 0xb
-	.uleb128 0x3e
-	.uleb128 0xb
-	.uleb128 0x3
-	.uleb128 0x8
-	.byte	0
-	.byte	0
-	.uleb128 0x4
-	.uleb128 0x1
-	.byte	0x1
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x1
-	.uleb128 0x13
-	.byte	0
-	.byte	0
-	.uleb128 0x5
-	.uleb128 0x21
-	.byte	0
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x2f
-	.uleb128 0x5
-	.byte	0
-	.byte	0
-	.uleb128 0x6
-	.uleb128 0x34
-	.byte	0
-	.uleb128 0x3
-	.uleb128 0xe
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0xb
-	.uleb128 0x39
-	.uleb128 0xb
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x3f
-	.uleb128 0x19
-	.uleb128 0x2
-	.uleb128 0x18
-	.byte	0
-	.byte	0
-	.uleb128 0x7
-	.uleb128 0x34
-	.byte	0
-	.uleb128 0x3
-	.uleb128 0x8
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0xb
-	.uleb128 0x39
-	.uleb128 0xb
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x3f
-	.uleb128 0x19
-	.uleb128 0x2
-	.uleb128 0x18
-	.byte	0
-	.byte	0
-	.uleb128 0x8
-	.uleb128 0x2e
-	.byte	0x1
-	.uleb128 0x3f
-	.uleb128 0x19
-	.uleb128 0x3
-	.uleb128 0xe
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0xb
-	.uleb128 0x39
-	.uleb128 0xb
-	.uleb128 0x49
-	.uleb128 0x13
-	.uleb128 0x11
-	.uleb128 0x1
-	.uleb128 0x12
-	.uleb128 0x7
-	.uleb128 0x40
-	.uleb128 0x18
-	.uleb128 0x2117
-	.uleb128 0x19
-	.uleb128 0x1
-	.uleb128 0x13
-	.byte	0
-	.byte	0
-	.uleb128 0x9
-	.uleb128 0x4109
-	.byte	0x1
-	.uleb128 0x11
-	.uleb128 0x1
-	.uleb128 0x31
-	.uleb128 0x13
-	.byte	0
-	.byte	0
-	.uleb128 0xa
-	.uleb128 0x410a
-	.byte	0
-	.uleb128 0x2
-	.uleb128 0x18
-	.uleb128 0x2111
-	.uleb128 0x18
-	.byte	0
-	.byte	0
-	.uleb128 0xb
-	.uleb128 0x2e
-	.byte	0
-	.uleb128 0x3f
-	.uleb128 0x19
-	.uleb128 0x3c
-	.uleb128 0x19
-	.uleb128 0x6e
-	.uleb128 0xe
-	.uleb128 0x3
-	.uleb128 0xe
-	.uleb128 0x3a
-	.uleb128 0xb
-	.uleb128 0x3b
-	.uleb128 0x5
-	.uleb128 0x39
-	.uleb128 0xb
-	.byte	0
-	.byte	0
-	.byte	0
-	.section	.debug_aranges,"",@progbits
-	.4byte	0x2c
-	.2byte	0x2
-	.4byte	.Ldebug_info0
-	.byte	0x8
-	.byte	0
-	.2byte	0
-	.2byte	0
-	.8byte	.LFB11
-	.8byte	.LFE11-.LFB11
-	.8byte	0
-	.8byte	0
-	.section	.debug_ranges,"",@progbits
-.Ldebug_ranges0:
-	.8byte	.LFB11
-	.8byte	.LFE11
-	.8byte	0
-	.8byte	0
-	.section	.debug_line,"",@progbits
-.Ldebug_line0:
-	.section	.debug_str,"MS",@progbits,1
-.LASF3:
-	.string	"long long int"
-.LASF1:
-	.string	"unsigned int"
-.LASF12:
-	.string	"main.c"
-.LASF14:
-	.string	"main"
-.LASF0:
-	.string	"long unsigned int"
-.LASF9:
-	.string	"long long unsigned int"
-.LASF11:
-	.string	"GNU C17 10.3.0 -mlittle-endian -mabi=lp64 -g -O2"
-.LASF4:
-	.string	"unsigned char"
-.LASF8:
-	.string	"char"
-.LASF15:
-	.string	"system"
-.LASF2:
-	.string	"long int"
-.LASF5:
-	.string	"short unsigned int"
-.LASF6:
-	.string	"signed char"
-.LASF13:
-	.string	"/usr/src/myapp"
-.LASF7:
-	.string	"short int"
-.LASF10:
-	.string	"tape"
 	.ident	"GCC: (GNU) 10.3.0"
 	.section	.note.GNU-stack,"",@progbits
