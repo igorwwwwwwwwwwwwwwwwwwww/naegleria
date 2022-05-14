69dd7abdcd9043bcfbc21106b0fe4614907665cb
igor <igor@igor.io>
2022-05-14 16:14:28 +0200

    first draft of wasm support

--- src/compiler.php
+++ src/compiler.php
@@ -17,137 +17,91 @@ function compile($tokens) {
     foreach ($tokens as $token) {
         switch ($token) {
             case '>';
-                yield ' # >';
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' ldr	w0, [x0]';
-                yield ' add	w1, w0, 1';
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' str	w1, [x0]';
+                yield '        ;; >';
+                yield '        (local.get 0)';
+                yield '        i32.const 1';
+                yield '        i32.add';
+                yield '        (local.set 0)';
                 break;
             case '<';
-                yield ' # <';
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' ldr	w0, [x0]';
-                yield ' sub	w1, w0, #1'; // this line changed
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' str	w1, [x0]';
+                yield '        ;; <';
+                yield '        (local.get 0)';
+                yield '        i32.const 1';
+                yield '        i32.sub';
+                yield '        (local.set 0)';
                 break;
             case '+';
-                yield ' # +';
-                yield ' adrp    x0, i';
-                yield ' add     x0, x0, :lo12:i';
-                yield ' ldr     w3, [x0]';
-                yield ' adrp    x0, tape';
-                yield ' add     x0, x0, :lo12:tape';
-                yield ' sxtw    x1, w3';
-                yield ' ldr     w0, [x0, x1, lsl 2]';
-                yield ' add     w2, w0, 1';
-                yield ' adrp    x0, tape';
-                yield ' add     x0, x0, :lo12:tape';
-                yield ' sxtw    x1, w3';
-                yield ' str     w2, [x0, x1, lsl 2]';
+                yield '        ;; +';
+                yield '        (local.get 0)';
+                yield '        (local.get 0)';
+                yield '        i32.load';
+                yield '        i32.const 1';
+                yield '        i32.add';
+                yield '        i32.store';
                 break;
             case '-';
-                yield ' # -';
-                yield ' adrp    x0, i';
-                yield ' add     x0, x0, :lo12:i';
-                yield ' ldr     w3, [x0]';
-                yield ' adrp    x0, tape';
-                yield ' add     x0, x0, :lo12:tape';
-                yield ' sxtw    x1, w3';
-                yield ' ldr     w0, [x0, x1, lsl 2]';
-                yield ' sub     w2, w0, #1'; // this line changed
-                yield ' adrp    x0, tape';
-                yield ' add     x0, x0, :lo12:tape';
-                yield ' sxtw    x1, w3';
-                yield ' str     w2, [x0, x1, lsl 2]';
+                yield '        ;; -';
+                yield '        (local.get 0)';
+                yield '        (local.get 0)';
+                yield '        i32.load';
+                yield '        i32.const 1';
+                yield '        i32.sub';
+                yield '        i32.store';
                 break;
             case '.';
-                yield ' # .';
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' ldr	w1, [x0]';
-                yield ' adrp	x0, tape';
-                yield ' add	x0, x0, :lo12:tape';
-                yield ' sxtw	x1, w1';
-                yield ' ldr	w0, [x0, x1, lsl 2]';
-                yield ' bl	putchar';
+                yield '        ;; .';
+                yield '        (i32.store (i32.const 0) (local.get 0))  ;; iov.iov_base';
+                yield '        (i32.store (i32.const 4) (i32.const 1))   ;; iov.iov_len';
+                yield '        (call $fd_write';
+                yield '            (i32.const 1) ;; file_descriptor - 1 for stdout';
+                yield '            (i32.const 0) ;; *iovs';
+                yield '            (i32.const 1) ;; iovs_len';
+                yield '            (i32.const 20) ;; nwritten';
+                yield '        )';
+                yield '        drop';
                 break;
             case ',';
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' ldr	w19, [x0]';
-                yield ' bl	getchar';
-                yield ' mov	w2, w0';
-                yield ' adrp	x0, tape';
-                yield ' add	x0, x0, :lo12:tape';
-                yield ' sxtw	x1, w19';
-                yield ' str	w2, [x0, x1, lsl 2]';
-                yield ' mov	w0, 0';
+                yield '        ;; ,';
+                // TBD
                 break;
             case '[';
                 $loopId++;
                 $loopStack[] = $loopId;
-                yield ' # [';
-                yield ".loops$loopId:";
-                yield ' adrp	x0, i';
-                yield ' add	x0, x0, :lo12:i';
-                yield ' ldr	w1, [x0]';
-                yield ' adrp	x0, tape';
-                yield ' add	x0, x0, :lo12:tape';
-                yield ' sxtw	x1, w1';
-                yield ' ldr	w0, [x0, x1, lsl 2]';
-                yield ' cmp	w0, 0';
-                yield " beq	.loope$loopId";
+                yield '        ;; [';
+                yield "        (block \$loope$loopId";
+                yield "        (loop \$loops$loopId";
+                // if val == 0, break
+                yield '        (local.get 0)';
+                yield '        i32.load';
+                yield '        i32.eqz';
+                yield '        (if (then';
+                yield "          br \$loope$loopId";
+                yield '        ))';
                 break;
             case ']';
                 $endLoopId = array_pop($loopStack);
-                yield ' # ]';
-                yield " b .loops$endLoopId";
-                yield ".loope$endLoopId:";
+                yield '        ;; ]';
+                yield "        br \$loops$endLoopId";
+                yield '        )';
+                yield '        )';
                 break;
         }
     }
 }
 
 const TEMPLATE = <<<'EOF'
-    .arch armv8-a
-    .text
-    .section	.rodata.str1.8,"aMS",@progbits,1
-    .align	3
-.LC0:
-    .string	"stty -icanon"
-    .text
-    .align	2
-    .global	main
-    .type	main, %function
-main:
-    stp	x29, x30, [sp, -16]!
-    mov	x29, sp
-    adrp	x0, .LC0
-    add	x0, x0, :lo12:.LC0
-    bl	system
+(module
+    (import "wasi_unstable" "fd_write" (func $fd_write (param i32 i32 i32 i32) (result i32)))
+    (memory 1)
+    (export "memory" (memory 0))
+    (data (i32.const 8) "") ;; tape begins at offset 8
+    (func $main (export "_start")
+        (local i32)
+        (local.set 0 (i32.const 8))
 $asm
-    mov	w0, 0
-    ldp	x29, x30, [sp], 16
-    ret
-    .size	main, .-main
-    .global	i
-    .global	tape
-    .bss
-    .align	3
-    .type	i, %object
-    .size	i, 8
-i:
-    .zero	8
-    .type	tape, %object
-    .size	tape, 20000
-tape:
-    .zero	20000
+    )
+)
+
 EOF;
 
 function template($asm) {
